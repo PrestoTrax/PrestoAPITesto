114d7c0bd7582f80f5fdb676bbee6d24803748ea
Max1mus7 <maxdweber7@gmail.com>
2022-03-05 23:17:44 -0700

    full remote unit testing

--- app.test.js
+++ app.test.js
@@ -1,7 +1,6 @@
+import { describe, jest } from '@jest/globals';
 
-import {describe, jest} from '@jest/globals';
-
-import index from "./index.js";
+import index from './index.js';
 const server = new index();
 
 jest.setTimeout(20000);
@@ -21,27 +20,22 @@ describe('User API tests', () => {
             code: 200,
             queryResult: [
                 {
-                    "Id": 8,
-                    "Username": "macks",
-                    "Email": "test@test.test",
-                    "Password": "test"
-                }
-            ]
+                    Id: 8,
+                    Username: 'macks',
+                    Email: 'test@test.test',
+                    Password: 'test',
+                },
+            ],
         });
     });
 
-    it('Adds a user to the DB', async() => {
+    it('Adds a user to the DB', async () => {
         const res = await server.newUser();
         expect(res.code).toBe(201);
         expect(res.message).toBe('Successfully added user to DB');
     });
 
     it('Should update the recently created user within the DB', async () => {
-        const updatedUser = {
-            "username": "newMacks",
-            "email": "newtest@newtest.newtest",
-            "password": "newtest"
-        };
         const res1 = await server.getUsers();
         const lastId = res1.queryResult.at(-1).Id;
         const res = await server.updateUser(lastId);
@@ -53,125 +47,96 @@ describe('User API tests', () => {
         const res1 = await server.getUsers();
         const lastId = res1.queryResult.at(-1).Id;
         const res = await server.deleteUser(lastId);
-        expect(res.code).toBe(204);
-        expect(res.data).toStrictEqual({});
+        expect(res).toBe(204);
     });
 });
-// //==================================================================================================================
-// //                                           Device Tests
-// //==================================================================================================================
-// describe('Device Info API tests', () => {
-//     it('Gets info of all devices within the DB', async () => {
-//         const res = await request(server).get('/devices');
-//         expect(res.code).toBe(200);
-//         //expect(res).toContain(res.queryResult);
-//     });
+//==================================================================================================================
+//                                           Device Tests
+//==================================================================================================================
+describe('Device Info API tests', () => {
+    it('Gets info of all devices within the DB', async () => {
+        const res = await server.getDevices();
+        expect(res.code).toBe(200);
+        //expect(res).toContain(res.queryResult);
+    });
 
-//     it('Gets info of a single specified device', async () => {
-//         const res = await request(server).get('/devices/2');
-//         expect(res.code).toBe(200);
-//         expect(res).toStrictEqual({
-//             code: 200,
-//             queryResult: [
-//                 {
-//                     "Id": 2,
-//                     "OwnerId": 8,
-//                     "DeviceLatitude": "33.512766 / N 33° 30' 45.956",
-//                     "DeviceLongitude": "-112.126330 / W 112° 7' 34.786",
-//                     "PingedAt": "2022-02-24T06:11:51.037Z",
-//                     "Moving": 0
-//                 }
-//             ]
-//         })
-//     });
+    it('Gets info of a single specified device', async () => {
+        const res = await server.getDevice(2);
+        expect(res.code).toBe(200);
+        expect(res).toStrictEqual({
+            code: 200,
+            queryResult: [
+                {
+                    Id: 2,
+                    OwnerId: 8,
+                    DeviceLatitude: "33.512766 / N 33° 30' 45.956",
+                    DeviceLongitude: "-112.126330 / W 112° 7' 34.786",
+                    PingedAt: '2022-02-24T06:11:51.037Z',
+                    Moving: 0,
+                },
+            ],
+        });
+    });
 
-//     it(`Gets all info of one user's devices`, async () => {
-//         const res = await request(server).get('/devices/user/8');
-//         expect(res.code).toBe(200);
-//     });
+    it(`Gets all info of one user's devices`, async () => {
+        const res = await server.getUserDevices(8);
+        expect(res.code).toBe(200);
+    });
 
-//     it('Adds a device to the DB', async () => {
-//         const newDevice = {
-//             "owner_id": 8,
-//             "location": {
-//                 "latitude": "33.512766 / N 33° 30' 45.956",
-//                 "longitude": "-112.126330 / W 112° 7' 34.786"
-//             },
-//             "moving": 0 
-//         }
-//         const res = await request(server).post('/devices/new').send(newDevice);
-//         expect(res.code).toBe(201);
-//         expect(res.message).toBe('Successfully added device to DB');
-//     });
-    
-//     it('Should update the recently created device within the DB', async () => {
-//         const updatedDevice = {
-//             "owner_id": "8",
-//             "location": {
-//                 "latitude": "34.512766 / N 33° 30' 45.956",
-//                 "longitude": "-113.126330 / W 112° 7' 34.786"
-//             },
-//             "moving": 0 
-//         };
-//         const res1 = await request(server).get('/devices');
-//         const lastId = res1.body.queryResult.at(-1).Id;
-//         const res = await request(server).put( `/devices/update/${lastId}`).send(updatedDevice);
-//         expect(res.code).toBe(201);
-//         expect(res.message).toBe('Successfully updated device within DB');
-//     });
+    it('Adds a device to the DB', async () => {
+        const res = await server.newDevice();
+        expect(res.code).toBe(201);
+        expect(res.message).toBe('Successfully added device to DB');
+    });
 
-//     it('Should delete the recently created device from the DB', async () => {
-//         const res1 = await request(server).get('/devices');
-//         const lastId = res1.body.queryResult.at(-1).Id;
-//         const res = await request(server).del(`/devices/delete/${lastId}`);
-//         expect(res.code).toBe(204);
-//         expect(res).toStrictEqual({});
-//     });
-// });
-// //==================================================================================================================
-// //                                           Record Tests
-// //==================================================================================================================
-// describe('Device Record API tests', () => {
-//     it('Gets info of all device records within the DB', async () => {
-//         const res = await request(server).get('/records');
-//         expect(res.code).toBe(200);
-//     });
+    it('Should update the recently created device within the DB', async () => {
+        const res1 = await server.getDevices();
+        const lastId = res1.queryResult.at(-1).Id;
+        const res = await server.updateDevice(lastId);
+        expect(res.code).toBe(201);
+        expect(res.message).toBe('Successfully updated device within DB');
+    });
 
-//     it('Gets info of a single specified record', async () => {
-//         const res = await request(server).get('/records/2');
-//         expect(res.code).toBe(200);
-//     });
+    it('Should delete the recently created device from the DB', async () => {
+        const res1 = await server.getDevices();
+        const lastId = res1.queryResult.at(-1).Id;
+        const res = await server.deleteDevice(lastId);
+        expect(res).toBe(204);
+    });
+});
+//==================================================================================================================
+//                                           Record Tests
+//==================================================================================================================
+describe('Device Record API tests', () => {
+    it('Gets info of all device records within the DB', async () => {
+        const res = await server.getRecords();
+        expect(res.code).toBe(200);
+    });
 
-//     it(`Gets all info of one user's devices`, async () => {
-//         const res = await request(server).get('/records/user/8');
-//         expect(res.code).toBe(200);
-//     });
-//     it(`Gets all of one device's records`, async () => {
-//         const res = await request(server).get('/records/device/8');
-//         expect(res.code).toBe(200);
-//     });
+    it('Gets info of a single specified record', async () => {
+        const res = await server.getRecord(2);
+        expect(res.code).toBe(200);
+    });
 
-//     it('Adds a device record to the DB', async () => {
-//         const newDevice = {
-//             "owner_id": 8,
-//             "parent_device": 1,
-//             "reported_lost": 0,
-//             "location": {
-//                 "latitude": "33.512766 / N 33° 30' 45.956",
-//                 "longitude": "-112.126330 / W 112° 7' 34.786"
-//             }
-//         }
-//         const res = await request(server).post('/records/new').send(newDevice);
-//         expect(res.code).toBe(201);
-//         expect(res.message).toBe('Successfully added device record to DB');
-//     });
-    
-//     it('Should delete the recently created device record within the DB', async () => {
-//         const res1 = await request(server).get('/devices');
-//         const lastId = res1.body.queryResult.at(-1).Id;
-//         const res = await request(server).del( `/records/delete/${lastId}`);
-//         expect(res.code).toBe(204);
-//         expect(res).toStrictEqual({});
-//     });
+    it(`Gets all info of one user's devices`, async () => {
+        const res = await server.getUserRecords(8);
+        expect(res.code).toBe(200);
+    });
+    it(`Gets all of one device's records`, async () => {
+        const res = await server.getDeviceRecords(8);
+        expect(res.code).toBe(200);
+    });
 
-// });
+    it('Adds a device record to the DB', async () => {
+        const res = await server.newRecord();
+        expect(res.code).toBe(201);
+        expect(res.message).toBe('Successfully added device record to DB');
+    });
+
+    it('Should delete the recently created device record within the DB', async () => {
+        const res1 = await server.getRecords();
+        const lastId = res1.queryResult.at(-1).Id;
+        const res = await server.deleteRecord(lastId);
+        expect(res).toBe(204);
+    });
+});
--- index.js
+++ index.js
@@ -41,10 +41,107 @@ class index {
         const response = await fetch(`https://prestoapi.azurewebsites.net/users/delete/${id}`, {
             method: "DELETE"
         });
-        if (response.status == 204)
-            return {code: 204, data: {}};
-        else
-            return FetchError;
+        return response.status;
+    }
+
+    async getDevices(){
+        const response = await fetch(`https://prestoapi.azurewebsites.net/devices`);
+        return await response.json();
+    }
+    async getDevice(id){
+        const response = await fetch(`https://prestoapi.azurewebsites.net/devices/${id}`);
+        return await response.json();
+    }
+    async getUserDevices(id){
+        const response = await fetch(`https://prestoapi.azurewebsites.net/devices/user/${id}`);
+        return await response.json();
+    }
+    async newDevice(){
+        const response = await fetch(`https://prestoapi.azurewebsites.net/devices/new`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "owner_id": 8,
+                    "location": {
+                        "latitude": "33.512766 / N 33° 30' 45.956",
+                        "longitude": "-112.126330 / W 112° 7' 34.786"
+                    },
+                    "moving": 0 
+                }),
+            });
+        return await response.json();
+    }
+    async updateDevice(id){
+        const response = await fetch(`https://prestoapi.azurewebsites.net/devices/update/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "owner_id": "8",
+                "location": {
+                    "latitude": "34.512766 / N 33° 30' 45.956",
+                    "longitude": "-113.126330 / W 112° 7' 34.786"
+                },
+                "moving": 0 
+            })
+        });
+        return await response.json();
+    }
+
+    async deleteDevice(id){
+        const response = await fetch(`https://prestoapi.azurewebsites.net/devices/delete/${id}`, {
+            method: 'DELETE'
+        });
+        return response.status;
+    }
+
+    async getRecords(){
+        const response = await fetch(`https://prestoapi.azurewebsites.net/records`);
+        return await response.json();
+    }
+
+    async getRecord(id){
+        const response = await fetch(`https://prestoapi.azurewebsites.net/records/${id}`);
+        return await response.json();
+    }
+
+    async getUserRecords(id){
+        const response = await fetch(`https://prestoapi.azurewebsites.net/records/user/${id}`);
+        return await response.json();
+    }
+
+    async getDeviceRecords(id){
+        const response = await fetch(`https://prestoapi.azurewebsites.net/records/device/${id}`);
+        return await response.json();
+    }
+
+    async newRecord(){
+        const response = await fetch(`https://prestoapi.azurewebsites.net/records/new`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "owner_id": 8,
+                "parent_device": 1,
+                "reported_lost": 0,
+                "location": {
+                    "latitude": "33.512766 / N 33° 30' 45.956",
+                    "longitude": "-112.126330 / W 112° 7' 34.786"
+                }
+            })
+        });
+        return await response.json();
+    }
+
+    async deleteRecord(id){
+        const response = await fetch(`https://prestoapi.azurewebsites.net/records/delete/${id}`, {
+            method: 'delete'
+        });
+        return response.status;
     }
 }
 
